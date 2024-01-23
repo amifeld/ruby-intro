f503e174a0ae01e9d3bb6406bef9f9a64d1b41ca
alan milstein <alan.milstein@kellogg.northwestern.edu>
2024-01-23 17:22:26 +0000

    change in code along 3 (loops)

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -8,4 +8,24 @@
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# set index variable
+index = 0
+
+#start loop
+loop do 
+    #break loop if number of elements is the same as the idex
+    if tacos.length == index
+        break
+    end
+
+    #assign a variable to each taco
+    taco = tacos[index]
+
+    #print the output
+    puts "Eat #{taco} tacos!"
+
+#increment the index
+index = index + 1
+end
